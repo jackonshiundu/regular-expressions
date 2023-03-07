cf2abcb7503cc73e790e1f2ab2f376191823e485
jackon shiundu <jackonshiundu2019@gmail.com>
2023-03-07 08:57:38 +0300

    first commit

---
+++ regularexpressions.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+   /*   . matches any character
+        * matches 0 or more
+        + matches 1 or more
+       the ? matches 0 or 1
+       <.*> greedy matching
+       <.*?> lazy matching/quntifieyer
+       {3} matches 
+       ie \d{3} will match any number with three digits
+       {3,} matches 3 or more
+       ie \d{3,5} will match any number that has three or five digits
+       {3} matches 3 or more
+       ie \d{3} will match any number with more tthan three digits
+       \s white space ,matches all the white spaces
+       \S non-white space ,matches all the non-white spaces
+       \d digit characters ,matches all the digit characters
+       \D non-digit characters ,matches all the non-digit characters
+       \w word characters ,matches all the word and numbers but not special characters
+       \W non-word characters ,matches spacial characters and white spaces
+       (a|b) matches either a or b 
+       [abc] matches a b and c 
+       [^abc] matches every charachter beside a,b or c
+       [a-z] matches all the range between a and z in small letters
+       [A-Z] matches all the range between A and Z in Capital letters
+       [0-9] matches all the digits
+       (hello) captures our match that is exactly the word hello
+       \escapes spacial meaning or characters
+       []-captures one item inside this
+       ()-groups and remembers content as an item
+       $ end of string
+       ^ beginning  of a string
+       - spacifies a range of characters
+    */
+    ?>
+
+</body>
+</html>
